a0cf3dc82e60fc3b1588b349abec1c09886ddaa8
Brendan Hansen <brendan.f.hansen@gmail.com>
2024-08-24 21:30:04 -0500

    switch to using catppuccin

--- lua/brendanfh/lazy/colors.lua
+++ lua/brendanfh/lazy/colors.lua
@@ -5,10 +5,12 @@ return {
 
     { "CantoroMC/nvim-nightmare" },
 
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
     {
         "miikanissi/modus-themes.nvim",
         config = function()
-            vim.cmd.colorscheme "kanagawa"
+            vim.cmd.colorscheme "catppuccin-mocha"
             vim.keymap.set("n", "<leader>cl", function() vim.o.background = "light" end)
             vim.keymap.set("n", "<leader>cd", function() vim.o.background = "dark" end)
         end
--- lua/brendanfh/lazy/mini.lua
+++ lua/brendanfh/lazy/mini.lua
@@ -3,7 +3,12 @@ return {
     version = '*',
     lazy = false,
 
+    dependencies = {
+        'echasnovski/mini.icons',
+    },
+
     config = function()
+        require('mini.icons').setup()
         require('mini.statusline').setup()
         require('mini.starter').setup {
             footer = "",
