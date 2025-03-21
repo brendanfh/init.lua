b04ee02ed35ffe59e1d15fa9f414d2677b18ba99
Brendan Hansen <brendan.f.hansen@gmail.com>
2025-03-21 15:02:26 -0500

    cleanup of old things

--- lua/brendanfh/lazy/colors.lua
+++ lua/brendanfh/lazy/colors.lua
@@ -1,31 +1,42 @@
 vim.keymap.set("n", "<leader>cl", function()
-    vim.cmd.colorscheme "newpaper"
+    -- vim.cmd.colorscheme "newpaper"
+    vim.cmd.colorscheme "xcodelight"
     vim.o.background = "light"
+    -- vim.cmd "hi Normal guibg=None"
 end)
 
 vim.keymap.set("n", "<leader>cd", function()
+    -- vim.cmd.colorscheme "catppuccin-mocha"
+    -- vim.cmd.colorscheme "xcodedarkhc"
     vim.cmd.colorscheme "kanagawa-dragon"
     vim.o.background = "dark"
+    -- vim.cmd "hi Normal guibg=None"
 end)
 
--- vim.cmd "hi Normal guibg=None"
 
 return {
-    { "rebelot/kanagawa.nvim"},
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            vim.cmd.colorscheme "kanagawa-dragon"
+        end
+    },
 
-    { "Mofiqul/vscode.nvim"},
+    -- { "Mofiqul/vscode.nvim"},
 
-    { "yorik1984/newpaper.nvim" },
+    -- { "yorik1984/newpaper.nvim" },
 
     -- { "iagorrr/noctis-high-contrast.nvim" },
 
     -- { "CantoroMC/nvim-nightmare" },
 
-    -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
     -- { "miikanissi/modus-themes.nvim" },
 
-    { "EdenEast/nightfox.nvim" },
+    -- { "EdenEast/nightfox.nvim" },
+
+    { "lunacookies/vim-colors-xcode" },
 
     -- {
     --     "RedsXDD/neopywal.nvim",
@@ -37,17 +48,17 @@ return {
     --     config = function() require "neopywal".setup {} end
     -- },
 
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require "tokyonight" .setup {
-                style = "night",
-                light_style = "day",
-                transparent = true,
-                terminal_colors = true
-            }
-        end
-    },
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function()
+    --         require "tokyonight" .setup {
+    --             style = "night",
+    --             light_style = "day",
+    --             transparent = true,
+    --             terminal_colors = true
+    --         }
+    --     end
+    -- },
 }
--- lua/brendanfh/lazy/filebrowser.lua
+++ lua/brendanfh/lazy/filebrowser.lua
@@ -1,10 +1,49 @@
+-- return {
+--     "nvim-telescope/telescope-file-browser.nvim",
+--     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+-- 
+--     config = function()
+--         require("telescope").load_extension "file_browser"
+-- 
+--         vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+--     end
+-- }
+
 return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-
-    config = function()
-        require("telescope").load_extension "file_browser"
-
-        vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-    end
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = { "folke/snacks.nvim", lazy = true },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>fb",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>-",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
